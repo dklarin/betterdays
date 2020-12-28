be719b3a2956b72328232f59e2fd3eb5723a3669
dklarin <danijel.klarin@gmail.com>
2020-12-28 21:12:05 +0100

    Updates

---
+++ precache-manifest.d01df2d6a82b5be694636db35d1f5a02.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd665197b66dedd4e5f2457e1f5aecee",
+    "url": "/betterdays/index.html"
+  },
+  {
+    "revision": "13dec774a2440f0f0a37",
+    "url": "/betterdays/static/css/2.e233df18.chunk.css"
+  },
+  {
+    "revision": "044c4ecd99d60c47ecc2",
+    "url": "/betterdays/static/css/main.84ab6231.chunk.css"
+  },
+  {
+    "revision": "13dec774a2440f0f0a37",
+    "url": "/betterdays/static/js/2.3458f0ab.chunk.js"
+  },
+  {
+    "revision": "4af38410273a2e197df7e24732830575",
+    "url": "/betterdays/static/js/2.3458f0ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044c4ecd99d60c47ecc2",
+    "url": "/betterdays/static/js/main.77260402.chunk.js"
+  },
+  {
+    "revision": "b3982794960a7fdbd786",
+    "url": "/betterdays/static/js/runtime-main.bce8cb3c.js"
+  },
+  {
+    "revision": "114592f29d7630ac399840270a584961",
+    "url": "/betterdays/static/media/calendar.114592f2.svg"
+  },
+  {
+    "revision": "5e75c209c7f0a5ed6b3a2770ddef732a",
+    "url": "/betterdays/static/media/down-arrow.5e75c209.svg"
+  },
+  {
+    "revision": "2ea4c43614e5b0beff1d450e9d280ea2",
+    "url": "/betterdays/static/media/search.2ea4c436.svg"
+  },
+  {
+    "revision": "b5cd24f24e481eb5246c5f1a6bc77bea",
+    "url": "/betterdays/static/media/up-arrow.b5cd24f2.svg"
+  }
+]);
